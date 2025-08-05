5dd7d640342f38ce9d12ba71eb05725a873c184f
mhmamundots <mhmamundots@gmail.com>
2025-08-06 03:06:23 +0600

    v1.0.0 Updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 'use strict';
+const searchMenu = document.querySelector('.search__menu');
+const searchBtnSelect = document.querySelector('.search__btn--select');
+const searchBtnItems = document.querySelectorAll('.search__btn--item');
+const searchText = document.querySelector('.search__text');
 
-const calcAge = birthYear => 2025 - birthYear;
+const searchBtnItem = document.querySelector('.search__btn--item');
 
-// BUG
-// FIXED
+searchBtnSelect.addEventListener('click', () => {
+  searchMenu.classList.toggle('search__menu--show');
+});
+
+searchBtnItems.forEach(item => {
+  item.addEventListener('click', e => {
+    const searchTitle =
+      e.currentTarget.querySelector('.search__title').innerText;
+    searchText.innerText = searchTitle;
+
+    searchMenu.classList.remove('search__menu--show');
+  });
+});
